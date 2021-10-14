25dd913738e796013e96b576cbefd17c9854d5a7
water <water>
2021-10-14 15:35:21 +0200

    asdf

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
 		<div class="max-w-3xl mx-auto text-center px-16 pt-8 pb-8 text-gray-800 dark:text-gray-200">
 			<a href="https://viscoin.net">Viscoin.net</a>
 			<p class="text-xs break-words-legacy">
-				{{ gitCommit }}
-				<br>
+				<!-- {{ gitCommit }}
+				<br> -->
 				{{ builtAt }}
 			</p>
 		</div>
