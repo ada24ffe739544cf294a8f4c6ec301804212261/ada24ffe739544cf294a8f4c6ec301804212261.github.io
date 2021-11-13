7f97bf473edadf1773cc62216f8e6ebe0160cb59
water <water>
2021-11-13 14:18:58 +0100

    Updates

---
+++ assets/litepaper.17219e8a.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.fb516a31.js";import{f as t,o as r,h as n}from"./vendor.309dbb5d.js";const s={mounted(){document.title="Viscoin.net - Litepaper"}};function a(c,p,d,f,i,l){const e=t("Object");return r(),n(e,{class:"w-full h-full",data:"/Viscoin.pdf",frameborder:"0"})}var u=o(s,[["render",a]]);export{u as default};
