120dd390b476d6a66b0f9c520c959d326635a387
water <water>
2021-11-13 14:44:10 +0100

    Updates

---
+++ assets/litepaper.340c335e.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.aa2c31b5.js";import{f as t,o as r,h as n}from"./vendor.309dbb5d.js";const a={mounted(){document.title="Viscoin.net - Litepaper"}};function c(s,p,d,i,f,l){const e=t("Object");return r(),n(e,{class:"w-full h-full",data:"/Viscoin.pdf",frameborder:"0"})}var u=o(a,[["render",c]]);export{u as default};
--- assets/litepaper.82588fc5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.1751090d.js";import{f as t,o as r,h as n}from"./vendor.309dbb5d.js";const s={mounted(){document.title="Viscoin.net - Litepaper"}};function c(a,p,d,i,f,l){const e=t("Object");return r(),n(e,{class:"w-full h-full",data:"/Viscoin.pdf",frameborder:"0"})}var u=o(s,[["render",c]]);export{u as default};
